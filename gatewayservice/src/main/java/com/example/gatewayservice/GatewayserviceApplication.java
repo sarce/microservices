e4840fd7fd30014cc0bd522d8dd719a6fb2f679c
Sebastián Arce <masearce@falabella.cl>
2018-05-09 17:35:41 -0300

    Configure gateway service with eureka service discovery.

--- gatewayservice/src/main/java/com/example/gatewayservice/GatewayserviceApplication.java
+++ gatewayservice/src/main/java/com/example/gatewayservice/GatewayserviceApplication.java
@@ -2,11 +2,13 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import com.example.gatewayservice.filters.pre.SimpleFilter;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayserviceApplication {
 
