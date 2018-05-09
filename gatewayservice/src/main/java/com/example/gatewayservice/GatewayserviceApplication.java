2fc889dab3c6110fb50e01bd7c978aeba8267dac
Sebastián Arce <masearce@falabella.cl>
2018-05-09 12:16:22 -0300

    New gateway service.

---
+++ gatewayservice/src/main/java/com/example/gatewayservice/GatewayserviceApplication.java
@@ -0,0 +1,21 @@
+package com.example.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import com.example.gatewayservice.filters.pre.SimpleFilter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserviceApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+}
